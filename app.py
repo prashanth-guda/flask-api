5eee9436896a32c946699055a6581e54cdf29911
prashanth <prashanthguda2@gmail.com>
2025-02-19 05:28:15 +0530

    connecting_db

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/login")
-def hello():
-    return "hello , prashanth!"
-
-
 @app.route("/")
-def homepage():
-    return "this is a home page"
+def hello():
+    return "hello , prashanth"
 
+@app.route("/profile" ,methods = ['post','get'])
+def profile():
+    if request.method == 'post':
+        return "hello"
+    else :
+        return "get me here"
 
+@app.route("/templete/<name>")
+def templet_render():
+    return render_template('hello.html',person = prasahnth)
 
 if __name__=="__main__":
     app.run(debug=True)
